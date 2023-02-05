8c9bd32c904577e2cb6018d9a575a9c7b18881b4
Gabriel Capizani <5434918-gabriel.capizani@users.noreply.gitlab.com>
2023-02-05 15:25:26 -0300

    feat: add s3 integrations and create user

--- api/Resources/users.py
+++ api/Resources/users.py
@@ -1,8 +1,34 @@
-from flask_restx import Resource
+from flask_restx import Resource, fields, reqparse
+from werkzeug.datastructures import FileStorage
 from config.api import api
 from api.Services.user import UserService
 
+
+create_user_parser = reqparse.RequestParser()
+create_user_parser.add_argument('username', type=str, location='form')
+create_user_parser.add_argument('email', type=str, location='form')
+create_user_parser.add_argument('avatar', type=FileStorage, location='files')
+
+
+responser_user = api.model('ResponseUser', {
+    "id": fields.Integer(),
+    "username": fields.String(),
+    "email": fields.String(),
+    "avatar_path": fields.String(),
+})
+
+
 @api.route('/users/<string:user_id>')
 class UserResource(Resource):
+    @api.marshal_with(responser_user)
     def get(self, user_id):
         return UserService.retrieve_by_id(user_id)
+
+
+@api.route('/users')
+class UserResource(Resource):
+    @api.marshal_with(responser_user)
+    @api.expect(create_user_parser)
+    def post(self):
+        args = create_user_parser.parse_args()
+        return UserService.create(args)
--- config/api.py
+++ config/api.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_restx import Api
 from flask_migrate import Migrate
+from dotenv import load_dotenv
 from config.db import SQLALCHEMY_DATABASE_URI, db
 
+load_dotenv() 
 
 app = Flask(__name__)
 api = Api(
