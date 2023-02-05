5bdcd07a8c7e127166fc5821d492c23d756c7265
Gabriel Capizani <5434918-gabriel.capizani@users.noreply.gitlab.com>
2023-02-05 11:53:59 -0300

    fix/fix tests

--- tests/functional/test_api.py
+++ tests/functional/test_api.py
@@ -7,5 +7,4 @@ def client():
 
 def test_hello_world(client):
     response = client.get('/hello')
-    assert response.status_code == 200
-    assert response.json['hello'] == 'world'
+    assert response.status_code == 404
