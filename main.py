8ccfa9d09aa496910f219dba060b0199e44c7246
Gabriel Capizani <5434918-gabriel.capizani@users.noreply.gitlab.com>
2023-02-14 20:04:01 -0300

    build-ci: add apm for tracing

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from config.api import app
+import logging
+from config.api import app, apm
 from api.Resources import users
+from elasticapm.handlers.logging import LoggingHandler
+
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True, port=8000)
+    handler = LoggingHandler(client=apm.client)
+    handler.setLevel(logging.INFO)
+    app.logger.addHandler(handler)
+    app.run(host="0.0.0.0", port=8000)
